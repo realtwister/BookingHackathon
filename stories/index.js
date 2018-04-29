47d921df7b7e8af27d5815fbf53ad6eb3fdbad5a
Erik Meulman <realtwister@gmail.com>
2018-04-29 14:33:19 +0200

    new design

--- stories/index.js
+++ stories/index.js
@@ -48,35 +48,47 @@ storiesOf('Slider', module)
   .add('Parent', ()=>(<Parent/>));
 
 var hotel = {
-    deep_link_url: 'booking://hotel/10007?checkout=2018-07-28&affiliate_id=881855&checkin=2018-07-27',
+    address: 'Keizersgracht 713 - 715, Amsterdam City Centre, Amsterdam',
+    review_score_word: 'Good',
+    hotel_currency_code: 'EUR',
+    postcode: '1017 DX',
     photo: 'https://q-xx.bstatic.com/images/hotel/max500_watermarked_standard_bluecom/79b/79b30a50ce232b91b4c5bf8cf6223e1653b3e9b4.jpg',
-    hotel_name: 'Armada Hotel',
     checkin_time: {
       from: '14:00',
       until: ''
     },
-    review_score_word: 'Good',
+    cc_required: true,
     price: 161,
     location: {
       longitude: '4.89844556897879',
       latitude: '52.3640545225574'
     },
-    hotel_id: 10007,
     hotel_url: 'https://www.booking.com/hotel/nl/armada.en.html?aid=881855&checkin=2018-07-27&checkout=2018-07-28&room1=A',
-    default_language: 'en',
-    hotel_currency_code: 'EUR',
-    address: 'Keizersgracht 713 - 715, Amsterdam City Centre, Amsterdam',
-    postcode: '1017 DX',
+    country: 'nl',
+    hotel_amenities: [
+      'paid_parking',
+      '24_hour_front_desk',
+      'non_smoking_rooms',
+      'internet_services',
+      'elevator',
+      'luggage_storage',
+      'wireless_lan',
+      'free_wifi_internet_access_included',
+      'all_public_and_private_spaces_non_smoking',
+      'private_parking'
+    ],
+    hotel_name: 'Armada Hotel',
+    hotel_id: 10007,
+    deep_link_url: 'booking://hotel/10007?checkout=2018-07-28&checkin=2018-07-27&affiliate_id=881855',
     review_score: 7.2,
-    stars: '3',
     cvc_required: false,
     review_nr: 1334,
-    country: 'nl',
-    cc_required: true
-  };
+    default_language: 'en',
+    stars: '3'
+};
 
 storiesOf('Card', module)
-  .add('Static', ()=>(<ListItem hotel={hotel} />))
+  .add('Static', ()=>(<div style={{width:750}}><ListItem hotel={hotel} /></div>));
 
 storiesOf('Popup', module)
 .add('example2', () => (
