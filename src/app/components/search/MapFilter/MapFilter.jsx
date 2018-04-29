202de16e9ab86303025ad0dc444dfc677c670d2e
Erik Meulman <realtwister@gmail.com>
2018-04-29 15:45:57 +0200

    centers

--- src/app/components/search/MapFilter/MapFilter.jsx
+++ src/app/components/search/MapFilter/MapFilter.jsx
@@ -14,10 +14,14 @@ class MapFilter extends React.Component {
   }
   render(){
     var {hotels, google, onSelect} = this.props;
+    var lat_cen = hotels.reduce((acum,hot)=>acum+parseFloat(hot.location.latitude),0)/hotels.length;
+    var lon_cen = hotels.reduce((acum,hot)=>acum+parseFloat(hot.location.longitude),0)/hotels.length;
+    var center= new google.maps.LatLng(lat_cen,lon_cen);
     return (
       <div className="mapFilter">
-      <Popup open={this.state.open} onClose = {this.togglePopup.bind(this)} onSelect={onSelect} hotels={hotels}/>
-      <Map className="smallMap" onClick={this.togglePopup.bind(this)} google={google} zoom={14} width="100%" height="100%" disableDefaultUI={true} options={()=>({panControl: false,
+      <Popup open={this.state.open} google={google} onClose = {this.togglePopup.bind(this)} onSelect={onSelect} hotels={hotels}/>
+      <Map className="smallMap" onClick={this.togglePopup.bind(this)}
+        center={center} google={google} zoom={14} width="100%" height="100%" disableDefaultUI={true} options={()=>({panControl: false,
         mapTypeControl: false, fullScreen:false, disableDefaultUI:true})}/>
 
     </div>
--- src/app/components/search/MapFilter/Popup.jsx
+++ src/app/components/search/MapFilter/Popup.jsx
@@ -3,6 +3,8 @@ import BestStylesHeet from "./Popup.scss"
 import GoogleMapDrawFilter from "react-google-map-draw-filter";
 import {MdClose} from 'react-icons/lib/md'
 
+import google from 'google-maps'
+
 class MyBestMap extends React.Component {
   constructor() {
     super();
@@ -19,17 +21,45 @@ calcMarkers() {
   }
   this.setState({markers});
 }
+getPoints(){
+
+}
+
+updateHeatmap(markers){
+
+  this.heatmap.set('data', this.getPoints());
+}
+getMapHandle(){
+  var ref = this.refs.fmapdraw
+  var gmaps = ref.state.google.maps;
+  var map = ref.state.map;
+  this.heatmap =  gmaps.visualization.HeatmapLayer({
+          data: this.getPoints(),
+          map: map
+        });
+}
 render() {
-  console.log(this.props);
-  // return null;
+  //setTimeout(this.getMapHandle.bind(this),1000)
+  var {hotels, google} = this.props;
+  var lat_cen = hotels.reduce((acum,hot)=>acum+parseFloat(hot.location.latitude),0)/hotels.length;
+      var lon_cen = hotels.reduce((acum,hot)=>acum+parseFloat(hot.location.longitude),0)/hotels.length;
+      var center= new google.maps.LatLng(lat_cen,lon_cen);
   return(
 <GoogleMapDrawFilter
+  ref="fmapdraw"
+  mapConfig={{
+		zoom:14,
+		lat:lat_cen,
+		lng:lon_cen,
+
+	}}
   width="100%" height="100%"
 apiKey='AIzaSyADYWSlC4yEedJ-5lvQb9UFOVaMMux54Zc'
 drawMode={true}
 markers={this.state.markers}
+heatMap = {true}
 mapStyle={{height: 400, width: 700}}
-handleReturnedMarkers={this.props.onSelect}
+handleReturnedMarkers={(args)=>{this.props.onSelect(args); this.updateHeatmap(args).bind(this)}}
 />
 );
   }
@@ -38,12 +68,13 @@ handleReturnedMarkers={this.props.onSelect}
 
 class Popup extends React.Component {
   render() {
+    var {google}=this.props;
     if (this.props.open){
     return (
       <div className='popup'>
         <div className='popup_inner'>
           <h1>{this.props.text}</h1>
-        <MyBestMap hotels={this.props.hotels} onSelect={this.props.onSelect} />
+        <MyBestMap hotels={this.props.hotels} onSelect={this.props.onSelect} google={google}/>
         <MdClose onClick={this.props.onClose} />
         </div>
       </div>
